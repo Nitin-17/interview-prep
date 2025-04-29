faf51ada19f41063f47092099af11e7508bd4f4a
nitin-17 <167026966+nitin-0018@users.noreply.github.com>
2025-04-29 15:48:01 +0530

    sign & signup added

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -17,6 +17,12 @@ import Link from "next/link";
 import { toast } from "sonner";
 import FormField from "./FormField";
 import { useRouter } from "next/navigation";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "@/firebase/client";
+import { signIn, signUp } from "@/lib/actions/auth.action";
 
 const authFormSchem = (type: FormType) => {
   return z.object({
@@ -41,13 +47,46 @@ const AuthForm = ({ type }: { type: FormType }) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       if (type === "sign-up") {
+        const { name, email, password } = values;
+
+        //this registers a new user in firebase authentication
+        const userCredentials = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const result = await signUp({
+          uid: userCredentials?.user?.uid,
+          name: name!,
+          email,
+          password,
+        });
+
+        if (!result?.success) {
+          toast.error(result?.message);
+          return;
+        }
+
         toast.success("Account Created Successfully");
         router.push("/sign-in");
       } else {
-        toast.success("Sign IN Successfully");
+        const { email, password } = values;
+        const userCredentials = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        const idToken = await userCredentials?.user?.getIdToken();
+        if (!idToken) {
+          toast.error("SignIn Failed");
+        }
+
+        await signIn({ email, idToken });
+        toast.success("Signed In Successfully");
         router.push("/");
       }
     } catch (error) {
